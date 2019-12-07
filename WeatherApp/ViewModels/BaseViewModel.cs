6c03fe879b03adbcfc220c6fce7f80a1e49e4ff6
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-07 19:18:13 +0000

    BaseViewModel and INotifyPropertyChanged

---
+++ WeatherApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WeatherApp.ViewModels
+{
+
+    /// <summary>
+    /// NotifyProperty interface allows class to notify the binding client that the property has changed value.
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetValue<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+                return;
+
+            backingField = value;
+
+            OnPropertyChanged(propertyName);
+        }
+    }
+}

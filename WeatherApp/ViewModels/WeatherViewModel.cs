c9034df67ec1ec9b1c419ee37a1777089ca58fbf
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-07 20:14:31 +0000

    WeatherAPI

--- WeatherApp/ViewModels/WeatherViewModel.cs
+++ WeatherApp/ViewModels/WeatherViewModel.cs
@@ -14,23 +14,16 @@ namespace WeatherApp.ViewModels
 
         public WeatherViewModel(Weather weather)
         {
-            // When initializing this object, I'm using the private field directly
-            // because I don't want this to raise a notification. 
+            // When initializing this object, I'm using the private field directly because I don't want this to raise a notification. 
 
-            // You may think we have violated the DRY principle (Don't Repeat 
-            // Yourself) because we've defined these properties in 2 places. 
+            // You may think we have violated the DRY principle (Don't Repeat Yourself) because we've defined these properties in 2 places. 
             // While that is partially true, we've done this for good reasons. 
-            // When you use the same class both as the domain and presentation model
-            // that class becomes bloated with too many responsibilities. This is
-            // not an issue for a small app (like this ContactBook app). But as
-            // the complexity of your application increases, throwing all these
-            // properties and methods into the same class will end up with a big 
-            // mess. So, it's better to have two separate models, one for presentation
-            // one for domain. This improves separation of concerns in your application
-            // but on the downside it comes with a cost: you have to repeat some of
-            // the properteis and map them together. While these few lines of 
-            // property assignment are not a major issue, if this still bothers
-            // you, you can use a convention-based mapping library like AutoMapper.
+            // When you use the same class both as the domain and presentation model that class becomes bloated with too many responsibilities. 
+            // This is not an issue for a small app (like this ContactBook app). 
+            // But as the complexity of your application increases, throwing all these properties and methods into the same class will end up with a big mess. 
+            //So, it's better to have two separate models, one for presentation one for domain. 
+            //This improves separation of concerns in your application but on the downside it comes with a cost: you have to repeat some of the properteis and map them together. 
+            // While these few lines of property assignment are not a major issue, if this still bothers you, you can use a convention-based mapping library like AutoMapper.
             Id = weather.Id;
             _cityName = weather.name;
             _icon = weather.weather[0].icon;
---
+++ WeatherApp/WeatherRestClient/WeatherAPI.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+using WeatherApp.ViewModels;
+
+namespace WeatherApp.WeatherRestClient
+{
+    class WeatherAPI : IWeatherRepository
+    {
+
+        private const string OpenWeatherApi = "http://api.openweathermap.org/data/2.5/weather?q=";
+        private const string Key = "653b1f0bf8a08686ac505ef6f05b94c2";
+        HttpClient _httpClient = new HttpClient();
+
+        public Task<IEnumerable<Weather>> GetSeveralCitiesWheaterAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Weather> GetWheater(string city)
+        {
+            var json = await _httpClient.GetStringAsync(OpenWeatherApi + city + "&APPID=" + Key);
+            var getWeatherModels = JsonConvert.DeserializeObject<Weather>(json);
+            return getWeatherModels;
+        }
+    }
+}
